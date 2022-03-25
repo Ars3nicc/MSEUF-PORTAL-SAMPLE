e3fad019c8b3057bb3eaa08075307c8ec8cc4d50
Ars3nicc <franzmanrique21@gmail.com>
2022-03-25 13:27:33 +0800

    Re-configured files

--- Portal_Info/models.py
+++ Portal_Info/models.py
@@ -1,3 +1,14 @@
+from msilib.schema import Class
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class PortalPage(models.Model):
+    StdInfo=models.TextField(max_length=200)
+    StdName=models.CharField(max_length=50)
+    StdID=models.IntegerField
+    StdSelection=models.IntegerField
+
+class PortalInformation(models.Model):
+    Homepage=models.TextField(max_length=200)
